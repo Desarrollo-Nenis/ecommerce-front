39973cb7eec7e70d26647e5139139ca17e72235a
Lu1sEnriquez <p-lenriquez@izzi.mx>
2025-04-26 10:29:00 -0700

    cambio interfaces nueva estructura

--- src/interfaces/categories/categories.interface.ts
+++ src/interfaces/categories/categories.interface.ts
@@ -1,25 +1,14 @@
 import { Img } from "../data/img.interface";
+import { Descuento } from "../products/products.interface";
 
 export interface Categoria {
-    id:             number;
-    documentId:     string;
-    nombre:         string;
-    createdAt:      Date;
-    updatedAt:      Date;
-    publishedAt:    Date;
-    subcategorias?: SubCategoria[];
-    img?:           Img;
-}
-
-export interface SubCategoria {
-    id:             number;
-    documentId:     string;
-    nombre:         string;
-    createdAt:      Date;
-    updatedAt:      Date;
-    publishedAt:    Date;
-    img?:           Img;
-}
-
+    id: number;
+    nombre: string;
+    img?: string;
+    principal?: Categoria;
+    subcategorias: Categoria[];
+    descuento?: Descuento;
+    slug: string;
+  }
 
 
--- src/interfaces/products/products.interface.ts
+++ src/interfaces/products/products.interface.ts
@@ -1,24 +1,70 @@
-import { Categoria, SubCategoria } from "../categories/categories.interface";
+import { Categoria } from "../categories/categories.interface";
+import { Marca } from "../marcas/marca.interface";
 
 
-export interface Products {
-    id:             number;
-    documentId:     string;
-    nombre:         string;
-    descripcion:    Descripcion[];
-    createdAt:      Date;
-    updatedAt:      Date;
-    publishedAt:    Date;
-    precioUnitario: number;
-    precioVenta:    number;
-    categorias:     Categoria[];
-    subcategorias:     SubCategoria[];
-    unidad_medida:  UnidadMedida;
-    cover?:          Cover;
-    galeria?:        Cover[];
+
+export interface Producto {
+    id: number;
+    nombre: string;
+    slug: string;
+    descripcion: Descripcion[]; // blocks (puede ser más específico según el editor usado)
+    categorias: Categoria[];    
+    marca?: Marca;
+    inventario: Inventario;
+    tipo: 'simple' | 'variante' | 'base';
+    variantes: Producto[];
+    principal?: Producto;
+    atributos?: AtributoSeleccionado[];
+    descuento?: Descuento;
+    cover?: string; // URL de la imagen
+    galeria: string[]; // URLs
     coverUrl: string
     galleryUrls?: string[]
-}
+  }
+  export interface Inventario {
+    stock: string;
+    precioCompra: string;
+    precioVenta: string;
+  }
+  export interface GrupoAtributo {
+    id: number;
+    nombre: string;
+    slug: string;
+  }
+
+  export interface AtributoSeleccionado {
+    id: number;
+    tipoAtributo: GrupoAtributo;
+    valor: string;
+  }
+
+  export interface Descuento {
+    id: number;
+    activo: boolean;
+    tipo: '$' | '%';
+    fechaInicio: string;
+    fechaFin: string;
+    valor: string;
+  }
+  
+// export interface Products {
+//     id:             number;
+//     documentId:     string;
+//     nombre:         string;
+//     descripcion:    Descripcion[];
+//     createdAt:      Date;
+//     updatedAt:      Date;
+//     publishedAt:    Date;
+//     precioUnitario: number;
+//     precioVenta:    number;
+//     categorias:     Categoria[];
+//     subcategorias:     SubCategoria[];
+//     // unidad_medida:  UnidadMedida;
+//     cover?:          Cover;
+//     galeria?:        Cover[];
+//     coverUrl: string
+//     galleryUrls?: string[]
+// }
 
 export interface UnidadMedida {
     id:            number;
