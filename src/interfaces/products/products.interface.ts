6ca6b2f2c0a527df1d59ac3fb6cb51f07f115e0d
Lu1sEnriquez <p-lenriquez@izzi.mx>
2025-04-29 20:35:15 -0700

    cambios en la estructura variantes -> producto[]

--- src/interfaces/products/products.interface.ts
+++ src/interfaces/products/products.interface.ts
@@ -1,52 +1,66 @@
 import { Categoria } from "../categories/categories.interface";
+import { Img } from "../data/img.interface";
 import { Marca } from "../marcas/marca.interface";
 
+export interface Products {
+  id: number;
+  nombre: string;
+  slug: string;
+  descripcion: Descripcion[]; // blocks (puede ser más específico según el editor usado)
+  categorias: Categoria[];
+  marca?: Marca;
+  inventario?: Inventario;
+  tipo:ProductType;
+  variantes: Products[];
+  principal?: Products;
+  atributos?: AtributoSeleccionado[];
+  descuento?: Descuento;
+  cover?: Img; // URL de la imagen
+  galeria: Img[]; // URLs
+  coverUrl: string;
+  galleryUrls?: string[];
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+}
+
+export enum ProductType {
+  SIMPLE = "simple",
+  BASE = "base",
+  VARIANT = "variante",
+}
 
 
-export interface Producto {
-    id: number;
-    nombre: string;
-    slug: string;
-    descripcion: Descripcion[]; // blocks (puede ser más específico según el editor usado)
-    categorias: Categoria[];    
-    marca?: Marca;
-    inventario: Inventario;
-    tipo: 'simple' | 'variante' | 'base';
-    variantes: Producto[];
-    principal?: Producto;
-    atributos?: AtributoSeleccionado[];
-    descuento?: Descuento;
-    cover?: string; // URL de la imagen
-    galeria: string[]; // URLs
-    coverUrl: string
-    galleryUrls?: string[]
-  }
-  export interface Inventario {
-    stock: string;
-    precioCompra: string;
-    precioVenta: string;
-  }
-  export interface GrupoAtributo {
-    id: number;
-    nombre: string;
-    slug: string;
-  }
+export interface Inventario {
+  stock: string;
+  precioCompra: number;
+  precioVenta: number;
+}
+export interface GrupoAtributo {
+  id?: number;
+  nombre: string;
+  values?: string[];
+}
 
-  export interface AtributoSeleccionado {
-    id: number;
-    tipoAtributo: GrupoAtributo;
-    valor: string;
-  }
+export interface AtributoSeleccionado {
+  id: number;
+  tipoAtributo: string;
+  valor: string;
+}
 
-  export interface Descuento {
-    id: number;
-    activo: boolean;
-    tipo: '$' | '%';
-    fechaInicio: string;
-    fechaFin: string;
-    valor: string;
-  }
-  
+export interface Descuento {
+  id: number;
+  activo: boolean;
+  tipo: DescuentoTipo;
+  fechaInicio: string;
+  fechaFin: string;
+  valor: string;
+}
+export enum DescuentoTipo {
+  PORCENTAJE = "%",
+  MONTO = "$",
+}
+
 // export interface Products {
 //     id:             number;
 //     documentId:     string;
@@ -67,62 +81,21 @@ export interface Producto {
 // }
 
 export interface UnidadMedida {
-    id:            number;
-    documentId:    string;
-    nombre?:       string;
-    createdAt:     Date;
-    updatedAt:     Date;
-    publishedAt:   Date;
-    nombreUnidad?: string;
-}
-
-export interface Cover {
-    id:                number;
-    documentId:        string;
-    name:              string;
-    alternativeText:   null;
-    caption:           null;
-    width:             number;
-    height:            number;
-    formats:           Formats;
-    hash:              string;
-    ext:               string;
-    mime:              string;
-    size:              number;
-    url:               string;
-    previewUrl:        null;
-    provider:          string;
-    provider_metadata: null;
-    createdAt:         Date;
-    updatedAt:         Date;
-    publishedAt:       Date;
-}
-
-export interface Formats {
-    thumbnail: Medium;
-    small:     Medium;
-    medium:    Medium;
-}
-
-export interface Medium {
-    name:        string;
-    hash:        string;
-    ext:         string;
-    mime:        string;
-    path:        null;
-    width:       number;
-    height:      number;
-    size:        number;
-    sizeInBytes: number;
-    url:         string;
+  id: number;
+  documentId: string;
+  nombre?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  nombreUnidad?: string;
 }
 
 export interface Descripcion {
-    type:     string;
-    children: Child[];
+  type: string;
+  children: Child[];
 }
 
 export interface Child {
-    type: string;
-    text: string;
+  type: string;
+  text: string;
 }
